1543247f082fbfdc86c2449f983d35f9df78025a
52ai <ieeflsyu@outlook.com>
2017-01-16 11:14:12 +0800

    已经发车，精准资助！

--- analysing_dorm_data.py
+++ analysing_dorm_data.py
@@ -4,17 +4,11 @@
 create on Jan 15,2017 by Wenyan Yu
 
 对寝室的门禁数据进行分析
-
 从初步的门禁数据分析可知，整个训练集共有2115065条门禁记录，有学生人数7834个
-
 计划对7834个学生做下面统计分析处理：
-
 先将原始数据处理成下面格式
-
 学生_ID(stu_id) , 日期(date_info), 最早出寝室时间(first_out), 最晚会宿舍时间(last_in)
-
 拟用两层字典（dict）加一个namedtuple来表示该信息
-
 {stu_id1: {date_info1: (first_out, last_out)
            date_info2: (first_out, last_out)
            date_info3: (first_out, last_out)
@@ -29,24 +23,17 @@ create on Jan 15,2017 by Wenyan Yu
         }
  ...
 }
-
 stu_id_dict = {}
 date_info_dict = {}
 DormRecord = namedtuple('DormRecord', ['first_out', 'last_in'])
 statistic_info = DormRecord(dt1.time(), dt2.time())
-
 统计之后的数据规模大概是
 7834 × 365 = 286160 (还可以吧)
-
 测试数据集如下：
 13126,"2014/01/21 03:31:11","1"
 13126,"2014/01/21 04:53:55","0"
-
 其中“1”表示出寝室，"0"表示进寝室
-
-
 处理数据的大致思路如下：
-
 1.读取测试数据集的一行数据
 2.将该行数据处理成stu_id, date_info, time_info, status
 3.判断stu_id是否存在于stu_id_dict中
@@ -65,7 +52,6 @@ statistic_info = DormRecord(dt1.time(), dt2.time())
             如果存在：
                 则判断status的状态，然后根据规则，修改first_out和last_in
 4.如此循环直至，训练集所有的记录都处理完成
-
 
 """
 
@@ -91,7 +77,6 @@ def store_structured_data(data_obj, file_to_store):
 def retrieve_structured_data(file_of_retrieve):
     """
     从文件中读取结构化数据
-    :param data_obj:
     :param file_of_retrieve:
     :return: data_obj
     """
@@ -105,6 +90,7 @@ def retrieve_structured_data(file_of_retrieve):
         return None
 
 
+def manipulation_date(file_to_manipulate, line_number):
 
 
 
